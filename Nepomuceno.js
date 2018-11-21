d6389509bd93a2c9c8dd70b284ab9faa038c9f88
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-11-21 21:07:49 +0800

    Fixed algo, File Reading left

--- Nepomuceno.js
+++ Nepomuceno.js
@@ -21,8 +21,13 @@ function calculateInterest(letters, format) {
   var dictionary = [], nopts = []
   nopts[start] = 1
   let lengthOfWord = format.length, noOfChars = letters.length
-  var word = []
+  var word = ''
   var option = []
+
+  for (let i = 0; i < noOfChars * 2; i++) {
+    option[i] = []
+  }
+
   while (nopts[start] > 0) { 												// while dummy stack is not empty
     if (nopts[move] > 0) {
       move++;
@@ -30,22 +35,17 @@ function calculateInterest(letters, format) {
 
       if (move == lengthOfWord + 1) {									// solution found
         for (i = 0; i < lengthOfWord; i++) {
-          // word[i] = letters.slice(option[i + 1][nopts[i + 1]] - 1);
-          // word[i] = word[i].toLowerCase()
-          // console.log(word[i])
-          console.log(letters.slice(option[i + 1][nopts[i + 1]] - 1))
+          word += letters[option[i + 1][nopts[i + 1]] - 1].toLowerCase();
         }
-        // console.log(word)
-        // word = []
+        console.log(word)
+        word = ''
         // for (i = 0; i < dictSize; i++)
         //   if (!stringComparison(word, dictionary[i]))
         //     printf("%s\n", word);
       }
       else if (move == 1) {											// only case where we'll populate the first position
         for (let candidate = noOfChars; candidate >= 1; candidate--) {
-          let plusone = ++nopts[move]
-          option[move] = []
-          option[move][plusone] = candidate;
+          option[move][++nopts[move]] = candidate;
         }
       }
       else {															// find candidates
@@ -53,7 +53,6 @@ function calculateInterest(letters, format) {
           for (var i = move - 1; i >= 1; i--)
             if (candidate == option[i][nopts[i]]) break;			// check top of stack of previous positions
           if (!(i >= 1)) {
-            option[move] = []
             option[move][++nopts[move]] = candidate;
           }												// meaning it did not break - candidate is not used by previous positions
         }
@@ -66,25 +65,22 @@ function calculateInterest(letters, format) {
     }
   }
 
-  // 	for(i = 0; i < dictSize; i++) free(dictionary[i]);
-  // }
-
   // void toLowerString(char* s) {
   // 	for (int i = 0; i < strlen(s); i++)
   // 		s[i] = tolower(s[i]);
   // }
 
-  // int stringComparison(char* s1, char* s2) {
-  // 	if(strlen(s1) != strlen(s2)) return 1;
-  // 	for(int i = 0; i < strlen(s1); i++) {
-  // 		if(s1[i] != '_' && s2[i] != '_') {
-  // 			if(s1[i] > s2[i]) return 1;
-  // 			else if(s1[i] < s2[i]) return -1;
-  // 		}
-  // 	}
-  // 	return 0;
-  // }
+  function stringComparison(s1, s2) {
+    if (s1.length != s2.length) return 1;
+    for (let i = 0; i < s1.length; i++) {
+      if (s1[i] != '_' && s2[i] != '_') {
+        if (s1[i] > s2[i]) return 1;
+        else if (s1[i] < s2[i]) return -1;
+      }
+    }
+    return 0;
+  }
 
 }
-calculateInterest("devil", "__");
+calculateInterest("devil", "___");
 
