7cf9fe2607f1a37f2b7d3eff967ab3c20410a094
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-11-22 13:14:38 +0800

    Configured Antd: working, Function working inside app.js

--- my-new-project/src/app.jsx
+++ my-new-project/src/app.jsx
@@ -1,10 +1,12 @@
+import { Button } from 'antd';
 import React from 'react';
 import calculateInterest from './Nepomuceno.js';
 export default class App extends React.Component {
   render() {
     return (<div>
       <h2>Gonna cry!</h2>
-      <button onClick={calculateInterest("boi", "__")}> hey </button>
+      <Button type="primary" onClick={() => calculateInterest("noot noot", "_noot_")}> hey </Button>
+
 
     </div>);
   }
