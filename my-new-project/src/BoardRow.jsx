dcef9cf121d0e36d1d910c743f00a688663bffab
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-11-24 03:00:48 +0800

    Works fine, check comments (Solver, Board) for problems, hardcoded answers at Frost22802

--- my-new-project/src/BoardRow.jsx
+++ my-new-project/src/BoardRow.jsx
@@ -5,7 +5,6 @@ import BoardCol from './BoardCol';
 export default class BoardRow extends React.Component {
     render() {
         var boardRow = this.props.row
-        console.log(boardRow)
         // console.log(this.state.letters)
         // console.log(this.state.board[0])
         return (
